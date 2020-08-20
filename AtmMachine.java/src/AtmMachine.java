f0f508c5206c2cd7902c970cca951603c120fbb2
johcasio <johnjofrey19@gmail.com>
2020-08-20 16:07:46 +0300

    joh. 21.8.2020

---
+++ AtmMachine.java/src/AtmMachine.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class AtmMachine {
+private static Scanner in;
+private static float balance =0;//initial balance for everyone
+    public static void main(String[] args) {
+	// write your code here
+        in =new Scanner(System.in);
+        //call our transaction method here
+        transaction();
+    }
+    private static void transaction(){
+        //here is where the most of work is
+        int choice;
+
+        System.out.println("please select an option");
+        System.out.println("1.Withdraw");
+        System.out.println("2.Deposit");
+        System.out.println("Balance");
+
+        choice =in.nextInt();
+
+        switch(choice){
+            case 1:
+            float amount;
+            System.out.println("please enter amount to withdraw:");
+            amount=in.nextFloat();
+            if(amount>balance || amount ==0){
+                System.out.println("you have insufficient funds\n\n");
+               
+
+            }
+            else
+            {
+                //they have some cash
+                //update balance
+                balance =balance - amount;
+                System.out.println(" you have withdrawn " + amount+"and your new balance is" +balance+"\n");
+    
+            }
+            break;
+
+            case 2:
+                //option number 2 is depositing
+                float deposit;
+                System.out.println(" please enter amount you wish to deposit:");
+                deposit = in.nextFloat();
+                //update balance
+                balance  = deposit + balance;
+                System.out.println(" you have deposited " +deposit+"new balance is"+balance+"\n");
+           
+                break;
+                
+            case 3:
+                // this option is to check balance
+               
+                System.out.println(" your balance is"+balance+"\n");
+            
+                break;
+
+        }
+    }//To change body of generated methods, choose Tools | Templates.
+    }
+
